b09e79378dd4c0950fb320ad3728f73a2cb2f3c6
Rivaldo Janter Tampubolon <114570416+yjm26@users.noreply.github.com>
2026-05-13 04:23:16 +0000

    Replace Circle UCW with Reown AppKit for wallet connection

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './config/reown'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
