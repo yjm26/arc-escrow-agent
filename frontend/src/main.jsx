8bd3466d175b7e79efd23e9b1f4162c536c7ccec
Rivaldo Janter Tampubolon <114570416+yjm26@users.noreply.github.com>
2026-05-13 04:28:12 +0000

    Remove side-effect import of reown config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './config/reown'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
